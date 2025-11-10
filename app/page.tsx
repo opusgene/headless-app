22628d2573aad7e44f3ad4b4f9524aa28793452b
Yanagihashi Fumiaki <ynghsfmak@gmail.com>
2025-11-10 12:01:44 +0900

    20251110_update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+);
 
 export default async function Home() {
-  const { data, error } = await supabase.from('posts').select('*')
-  console.log(data, error)
+  const { data, error } = await supabase.from("posts").select("*");
+  console.log(data, error);
 
   return (
     <main className="p-8">
       <h1 className="text-xl font-bold">Hello Headless CMS</h1>
       <p>Supabase接続テスト完了！</p>
+      <div className="text-red-500 text-3xl font-bold">Tailwind動いてる？</div>
     </main>
-  )
+  );
 }
