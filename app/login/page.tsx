2f96ff7985ba7b654114d1aca2425cea508db3aa
Yanagihashi Fumiaki <ynghsfmak@gmail.com>
2026-03-20 20:03:55 +0900

    20260223_update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import { useImpersonate } from "@/context/impersonateContext";
+import "../globals.css";
 
 export default function LoginPage() {
   const router = useRouter();
